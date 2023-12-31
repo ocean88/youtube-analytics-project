9da65ae36950f3493dd3c3ccdc88f96ba677447e
ocean88 <oceann3@gmail.com>
2023-12-31 13:50:26 +0900

    домашка 4

--- src/video.py
+++ src/video.py
@@ -1,12 +1,30 @@
+import os
+from dotenv import load_dotenv
+from googleapiclient.discovery import build
+
+
+load_dotenv()
+
+api_key = os.environ.get('API_KEY')
+youtube = build('youtube', 'v3', developerKey=api_key)
+
+
 class Video:
     def __init__(self, video_id):
         self.video_id = video_id
+        self.title = None
 
     def __str__(self):
-        video_titles = {
-            'AWX4JnAnjBE': 'GIL в Python: зачем он нужен и как с этим жить'
-        }
-        return video_titles.get(self.video_id, 'Unknown Video')
+        if self.title is None:
+            self._fetch_title()
+        return self.title
+
+    def _fetch_title(self):
+        video_response = youtube.videos().list(
+            part='snippet',
+            id=self.video_id
+        ).execute()
+        self.title = video_response['items'][0]['snippet']['title']
 
 
 class PLVideo(Video):
@@ -15,13 +33,10 @@ class PLVideo(Video):
         self.playlist_id = playlist_id
 
     def __str__(self):
-        video_titles = {
-            '4fObz_qw9u4': 'MoscowPython Meetup 78 - вступление'
-        }
-        return video_titles.get(self.video_id, 'Unknown Video')
+        if self.title is None:
+            self._fetch_title()
+        return self.title
 
 
-# тест функций
-
-print(Video('AWX4JnAnjBE'))
-print(PLVideo('4fObz_qw9u4', 'PLv_zOGKKxVph_8g2Mqc3LMhj0M_BfasbC'))
+video1 = Video('AWX4JnAnjBE')  # 'AWX4JnAnjBE' - это id видео из ютуб
+video2 = PLVideo('4fObz_qw9u4', 'PLv_zOGKKxVph_8g2Mqc3LMhj0M_BfasbC')
